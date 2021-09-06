37da33ea0544caa8723760e3ed312589e7267219
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-09-07 02:25:11 +0700

    update frontend conf

--- auto-scale-startup.sh
+++ auto-scale-startup.sh
@@ -8,7 +8,8 @@ sudo usermod -a -G docker ec2-user
 docker info
 
 # Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # Verify Installation
 docker-compose version
@@ -17,4 +18,4 @@ docker-compose version
 sudo yum install git -y
 
 # Clone the repository
-
+git clone https://github.com/rmit-cloud-computing/deployment.git
